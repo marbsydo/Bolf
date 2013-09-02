86e56993f28978345ee2143dd878e90f97d114c4
Sam Hooke <marbsam@yahoo.co.uk>
2013-09-02 12:06:23 +0100

    Can roll ball with mouse/touch

--- Assets/Main/GameControls.cs
+++ Assets/Main/GameControls.cs
@@ -2,14 +2,20 @@ using UnityEngine;
 using System.Collections;
 
 enum BallStage {DoesNotExist, AwaitingRelease, InMotion};
+enum ReleaseStage {NoInput, MovingBallSideways, SwipingTowardsBall, SwipingAwayFromBall, Released}
 
 public class GameControls : MonoBehaviour {
 	Object ballPrefab;
 	Ball ball;
 	BallStage ballStage = BallStage.DoesNotExist;
+	ReleaseStage releaseStage = ReleaseStage.NoInput;
 
 	Vector3[] markers;
 	Vector3 markersMid;
+	float markersLine;
+
+	Vector3 swipeFrom;
+	Vector3 swipeTo;
 
 	void Awake() {
 		ballPrefab = Resources.Load("Ball");
@@ -19,6 +25,7 @@ public class GameControls : MonoBehaviour {
 		markers[0] = GameObject.Find("BallMarkerLeft").transform.position;
 		markers[1] = GameObject.Find("BallMarkerRight").transform.position;
 		markersMid = markers[0] + (markers[1] - markers[0]) / 2;
+		markersLine = markersMid.y + 1.5f;
 	}
 
 	void Start() {
@@ -27,15 +34,62 @@ public class GameControls : MonoBehaviour {
 
 	void Update() {
 		if (ballStage == BallStage.AwaitingRelease) {
-			// Move ball left/right with touch
+
+			Vector3 m, ms;
+
+			if (Input.GetMouseButtonDown(0)) {
+				if (releaseStage == ReleaseStage.Released) {
+					// DEBUGGING: reset ball when touch after released
+					ResetBall();
+					releaseStage = ReleaseStage.NoInput;
+				}
+			}
+
 			if (Input.GetMouseButton(0)) {
-				Vector3 t = ball.transform.position;
-				t.x = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z)).x;
-				if (t.x < markers[0].x)
-					t.x = markers[0].x;
-				if (t.x > markers[1].x)
-					t.x = markers[1].x;
-				ball.transform.position = t;
+				m = Input.mousePosition;
+				ms = Camera.main.ScreenToWorldPoint(new Vector3(m.x, m.y, -Camera.main.transform.position.z));
+
+				if (releaseStage == ReleaseStage.NoInput) {
+					if (ms.y < markersLine) {
+						// Move ball left/right with touch
+						Vector3 t = ball.transform.position;
+						t.x = ms.x;
+						if (t.x < markers[0].x)
+							t.x = markers[0].x;
+						if (t.x > markers[1].x)
+							t.x = markers[1].x;
+						ball.transform.position = t;
+					} else {
+						// Begin swiping towards ball
+						releaseStage = ReleaseStage.SwipingTowardsBall;
+						swipeFrom = ms;
+					}
+				} else if (releaseStage == ReleaseStage.SwipingTowardsBall) {
+					if ((ms - ball.transform.position).magnitude < 1f) {
+						// Swiped onto ball, so begin swiping away
+						releaseStage = ReleaseStage.SwipingAwayFromBall;
+					}
+				}
+			}
+
+			if (Input.GetMouseButtonUp(0)) {
+				m = Input.mousePosition;
+				ms = Camera.main.ScreenToWorldPoint(new Vector3(m.x, m.y, -Camera.main.transform.position.z));
+
+				if (releaseStage == ReleaseStage.SwipingAwayFromBall) {
+					// Release ball
+					swipeTo = ms;
+
+					// aim towards the average of swipeFrom and swipeTo
+					Vector3 swipeMean = (swipeFrom + swipeTo) / 2;
+
+					BallRoll(swipeMean);
+
+					releaseStage = ReleaseStage.Released;
+
+				} else if (releaseStage == ReleaseStage.SwipingTowardsBall) {
+					releaseStage = ReleaseStage.NoInput;
+				}
 			}
 		}
 	}
@@ -59,7 +113,7 @@ public class GameControls : MonoBehaviour {
 
 	void DestroyBall() {
 		if (ball != null) {
-			Destroy(ball.transform);
+			Destroy(ball.gameObject);
 			ballStage = BallStage.DoesNotExist;
 		}
 	}
@@ -83,12 +137,8 @@ public class GameControls : MonoBehaviour {
 			BallRoll(Input.mousePosition);
 		}
 	}
-
-	void BallRoll(Vector3 touch) {
-		Vector2 touchPosWorld = Camera.main.ScreenToWorldPoint(new Vector3(touch.x, touch.y, -Camera.main.transform.position.z));
-
-		Vector2 diff = touchPosWorld - (Vector2)ball.transform.position;
-		ball.Roll(diff);
-	}
 */
+	void BallRoll(Vector3 pos) {
+		ball.Roll((Vector2)pos - (Vector2)ball.transform.position);
+	}
 }
