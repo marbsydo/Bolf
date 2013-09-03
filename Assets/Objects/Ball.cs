add7ecab5d4a856398d47dd9e71e96216c51d477
Sam Hooke <marbsam@yahoo.co.uk>
2013-09-03 12:17:05 +0100

    Added mega bumper

--- Assets/Objects/Ball.cs
+++ Assets/Objects/Ball.cs
@@ -5,6 +5,10 @@ public class Ball : MonoBehaviour {
 
 	GameObject holes;
 
+	float velocityThreshold1 = 15f;
+	float velocityThreshold2 = 20f;
+	float velocityThreshold3 = 25f;
+
 	public void Awake() {
 		Transform[] children = gameObject.GetComponentsInChildren<Transform>();
 		foreach (Transform child in children) {
@@ -16,6 +20,19 @@ public class Ball : MonoBehaviour {
 
 	public void Update() {
 		holes.transform.Rotate(((new Vector3(rigidbody.velocity.y, 0f, -rigidbody.velocity.x)).normalized) * (rigidbody.velocity.magnitude * 50f * Time.deltaTime));
+
+		Debug.Log(rigidbody.velocity.magnitude);
+
+		float v = rigidbody.velocity.magnitude;
+		if (v > velocityThreshold3) {
+			rigidbody.drag = 2f;
+		} else if (v > velocityThreshold2) {
+			rigidbody.drag = 0.5f;
+		} else if (v > velocityThreshold1) {
+			rigidbody.drag = 0.05f;
+		} else {
+			rigidbody.drag = 0f;
+		}
 	}
 
 	public void Roll(Vector2 v) {
