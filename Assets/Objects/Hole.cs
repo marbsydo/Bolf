1ffcaa359903947e3e42d94494c23c6cf1890cfe
Sam Hooke <marbsam@yahoo.co.uk>
2013-09-03 15:52:00 +0100

    Improved chipping code
    Removed old commented out code. Raised the velocity threshold so it is
    easier to get a ball in the hole.

--- Assets/Objects/Hole.cs
+++ Assets/Objects/Hole.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class Hole : MonoBehaviour {
 
+	const float fallInHoleVelocityThreshold = 8f;
+
 	float timeLastChipBall = 0;
 
 	void OnTriggerStay(Collider collider) {
@@ -16,32 +18,17 @@ public class Hole : MonoBehaviour {
 				forceVector = diff.normalized * -0.1f * 1/Mathf.Max(diff.magnitude, 1) * (Time.deltaTime * 40);
 				collider.rigidbody.AddForce(forceVector, ForceMode.VelocityChange);
 			}
-			if (diff.magnitude < 0.5f) {
+			if (diff.magnitude < 0.7f) {
 				Ball ball = collider.GetComponent<Ball>();
-				if (ball.rigidbody.velocity.magnitude < 5f) {
+				if (ball.rigidbody.velocity.magnitude < fallInHoleVelocityThreshold) {
 					ball.GoInHole(this);
 				} else {
 					// Too fast, so chip off some speed and knock the ball a bit
 					if (Time.timeSinceLevelLoad > timeLastChipBall + 1f) {
 						timeLastChipBall = Time.timeSinceLevelLoad;
-						//ball.rigidbody.AddForce(new Vector3(Random.value, Random.value, 0) * 10000000f, ForceMode.VelocityChange);
-						//ball.rigidbody.velocity = ball.rigidbody.velocity + new Vector3(Random.value, Random.value, 0f) * 10f;
-						//ball.rigidbody.velocity = RotateAroundPoint(ball.rigidbody.velocity, Vector3.zero, Quaternion.Euler(0, 90, 0));
-						//ball.rigidbody.velocity *= 0.8f;
-						//ball.rigidbody.MoveRotation(Quaternion.Euler(0, 0, 45));
 
-						float t = ball.rigidbody.velocity.magnitude;
-						//ball.rigidbody.AddRelativeForce(new Vector3(Random.value, Random.value, 0f) * -10f, ForceMode.VelocityChange);
-						float chipAngle;
-						chipAngle = 45 + Random.value * 45;
-						if (Random.value < 0.5)
-							chipAngle *= -1;
-
+						float chipAngle = 20 + Random.value * 20 * (Random.value < 0.5f ? -1 : 1);
 						ball.rigidbody.velocity = Quaternion.AngleAxis(chipAngle, Vector3.forward) * ball.rigidbody.velocity;
-						if (ball.rigidbody.velocity.magnitude > t) {
-							ball.rigidbody.velocity = ball.rigidbody.velocity.normalized * t;
-						}
-						Debug.Log("CHIP! " + Random.value);
 					}
 				}
 			}
