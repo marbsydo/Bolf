f1ed27a23f45f460dfdf6696bb2634cc64924fca
Sam Hooke <marbsam@yahoo.co.uk>
2013-09-09 17:41:50 +0100

    Removed unnecessary stuff

--- Assets/Main/GameControls.cs
+++ Assets/Main/GameControls.cs
@@ -53,26 +53,16 @@ public class GameControls : MonoBehaviour {
 
 	ReleaseStage releaseStage = ReleaseStage.Positioning;
 
-	Vector3[] markers;
-	Vector3 markersMid;
-	float markersLine;
-
-	float chosenX;
-
-	const float ballScalingModifier = 0.5f; // This is how much bigger the ball gets e.g. 1.5 = 150%
-	const float ballSinModifier = 2f;
-
 	SwipeSensor swipeSensor = new SwipeSensor();
+
+	Vector3 ballStartPosition;
 
 	void Awake() {
 		ballPrefab = Resources.Load("Ball");
 
-		// Find the two markers
-		markers = new Vector3[2];
-		markers[0] = GameObject.Find("BallMarkerLeft").transform.position;
-		markers[1] = GameObject.Find("BallMarkerRight").transform.position;
-		markersMid = markers[0] + (markers[1] - markers[0]) / 2;
-		markersLine = markersMid.y + 1.62f;
+		GameObject temp = GameObject.Find("Ball") as GameObject;
+		ballStartPosition = temp.transform.position;
+		Destroy(temp);
 	}
 
 	void Start() {
@@ -109,6 +99,7 @@ public class GameControls : MonoBehaviour {
 			mPos = Input.mousePosition;
 			mPosWorld = Camera.main.ScreenToWorldPoint(new Vector3(mPos.x, mPos.y, -Camera.main.transform.position.z));
 
+			// If the ball is in the hole, touch to reset
 			if (Input.GetMouseButtonDown(0)) {
 				if (ball.IsInHole()) {
 					ResetBall();
@@ -116,10 +107,12 @@ public class GameControls : MonoBehaviour {
 				}
 			}
 
+			// If touching, add point to swipeSensor
 			if (Input.GetMouseButton(0)) {
 				swipeSensor.AddPoint((Vector2) mPosWorld);
 			}
 
+			// If released touch, fire if necessary
 			if (Input.GetMouseButtonUp(0)) {
 				if (lineExists) {
 					Vector3 forceVector = ((Vector3) lineVector.normalized) * (lineVector.magnitude / Mathf.Max(0.1f, lineTime));
@@ -127,87 +120,12 @@ public class GameControls : MonoBehaviour {
 				}
 				swipeSensor.ResetPoints();
 			}
-
-
-			/*
-			// Ball movement
-
-			if (releaseStage == ReleaseStage.Positioning || releaseStage == ReleaseStage.PulledBack) {
-				Vector3 newBallPos = ball.transform.position;
-
-				if (Input.GetMouseButton(0)) {
-					if (mPosWorld.y < markersLine - 1.5f) {
-						// Mouse button is held down at the bottom of the screen, so switch to PulledBack
-						releaseStage = ReleaseStage.PulledBack;
-						chosenX = ball.transform.position.x;
-					}
-
-					if (releaseStage == ReleaseStage.Positioning) {
-						// Mouse button is held down while in Positioning stage, so move ball in X position across markersLine
-						newBallPos.x = mPosWorld.x;
-						newBallPos.x = Mathf.Max(newBallPos.x, markers[0].x + 0.5f);
-						newBallPos.x = Mathf.Min(newBallPos.x, markers[1].x - 0.5f);
-					}
-				}
-
-				if (releaseStage == ReleaseStage.Positioning) {
-					newBallPos.y = Mathf.Lerp(newBallPos.y, markersLine, Time.deltaTime * 5f);
-				}
-
-				// Release stage is PulledBack, so pull the ball back at the chosen position chosenX
-				// Also record the mouse movement in order to detect a swipe
-				if (releaseStage == ReleaseStage.PulledBack) {
-					newBallPos.x = chosenX;
-					newBallPos.y = Mathf.Lerp(newBallPos.y, markersLine - 2f, Time.deltaTime * 5f);
-					swipeSensor.AddPoint((Vector2) mPosWorld);
-				}
-
-				if (Input.GetMouseButtonUp(0)) {
-					if (releaseStage == ReleaseStage.PulledBack) {
-						if (mPosWorld.y <= markersLine) {
-							// Mouse was released within positioning area, so revert back to Positioning
-							releaseStage = ReleaseStage.Positioning;
-						} else {
-							// Mouse was released outside of positioning area, so fire if necessary
-							if (lineExists) {
-								// A straight line does exist, so fire the ball
-								Vector3 forceVector = ((Vector3) lineVector.normalized) * (lineVector.magnitude / Mathf.Max(0.1f, lineTime));
-								ball.rigidbody.AddForce(forceVector, ForceMode.Impulse);
-								releaseStage = ReleaseStage.Released;
-							} else {
-								// A straight line does not exist, so return to positioning
-								releaseStage = ReleaseStage.Positioning;
-							}
-							swipeSensor.ResetPoints();
-						}
-					}
-				}
-
-				// Update ball's position
-				ball.transform.position = newBallPos;
-				ball.transform.localScale = CalculateBallScale();
-			}
-
-			if (releaseStage == ReleaseStage.Released) {
-
-				ball.transform.localScale = Vector3.Lerp(ball.transform.localScale, Vector3.one, Time.deltaTime * 5f);
-
-				if (Input.GetMouseButtonDown(0)) {
-					ResetBall();
-					releaseStage = ReleaseStage.Positioning;
-				}
-			}
-			*/
 		}
-	}
-
-	Vector3 CalculateBallScale() {
-		return Vector3.one * (1 + Mathf.Sin(Mathf.Max(0f, markersLine - ball.transform.position.y) / ballSinModifier) * ballScalingModifier);
 	}
 
 	void ResetBall() {
 		DestroyBall();
-		CreateBall(markersMid);
+		CreateBall(ballStartPosition);
 	}
 
 	void CreateBall(Vector3 p) {
