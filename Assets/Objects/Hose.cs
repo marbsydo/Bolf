930ed21ec1d5cc0840057fd5a364609a1161a1c4
Sam Hooke <marbsam@yahoo.co.uk>
2013-09-13 21:54:21 +0100

    bulges travel down hose at any angle

--- Assets/Objects/Hose.cs
+++ Assets/Objects/Hose.cs
@@ -2,32 +2,79 @@ using UnityEngine;
 using System.Collections;
 
 public class Hose : MonoBehaviour {
-	float startAngle;
 
-	bool gotWater = true;
+	// You can change these three:
+	const int numBulges = 10; // How many bulges to make
+	const float spacing = 2f; // How far apart they are spread
+	const float speed = 3f;   // Speed the bulges move at
 
-	WaterJet waterJet = new WaterJet(8f, 200f, true);
+	// Do not set these
+	// These are set from reading transform.eulerAngles.z
+	float angle;
+	float angleCos;
+	float angleSin;
+	Vector3 spacingVector;
+	Vector3 spacingVectorNormalized;
+
+	GameObject[] bulges;
 
 	void Awake() {
-		waterJet.Init();
-		startAngle = transform.eulerAngles.z;
+		Object bulgePrefab = Resources.Load("Sprites/HoseBulgeSprite");
+		bulges = new GameObject[numBulges];
+
+		angle = transform.eulerAngles.z * Mathf.Deg2Rad;
+		angleCos = Mathf.Cos(angle);
+		angleSin = Mathf.Sin(angle);
+
+		spacingVector = new Vector3(spacing * angleCos, spacing * angleSin, 0f);
+		spacingVectorNormalized = spacingVector.normalized;
+
+		Quaternion rot = Quaternion.Euler(0f, 0f, transform.eulerAngles.z);
+
+		for (int i = 0; i < bulges.Length; i++) {
+			float offset = i * spacing;
+			bulges[i] = GameObject.Instantiate(bulgePrefab, transform.position + spacingVector * i, rot) as GameObject;
+		}
 	}
 
 	void Update() {
-		GameObject ball = GameObject.Find("Ball") as GameObject;
+		float step = speed * -Time.deltaTime;
+		Vector3 bulgeStep = new Vector3(step * angleCos, step * angleSin, 0f);
 
-		if (ball != null) {
-			// Rotate towards the ball
-			float angle = Mathf.Atan2(ball.transform.position.y - transform.position.y, ball.transform.position.x - transform.position.x) * Mathf.Rad2Deg;
-			transform.eulerAngles = new Vector3(0f, 0f, angle);
+		GameObject furthest = Furthest();
 
-			// Spray the ball
-			if (gotWater) {
-				Vector3 jetOrigin;
-				Vector3 jetVector = waterJet.EulerAngleToNormalizedVector(angle);
-				jetOrigin = transform.position + jetVector * 0.5f;
-				waterJet.Spray(jetOrigin, jetVector);
+		for (int i = 0; i < bulges.Length; i++) {
+			bulges[i].transform.position += bulgeStep;
+
+			Vector3 vectorToHoseNormalized = (transform.position - bulges[i].transform.position).normalized;
+
+			if (ApproximatelyEqual(vectorToHoseNormalized, spacingVectorNormalized)) {
+				bulges[i].transform.position = furthest.transform.position + spacingVector;
 			}
 		}
 	}
+
+	// Compares if two vector3s are approximately equal
+	// NOTE: DOES NOT COMPARE THE Z! (this is intended, just fyi)
+	bool ApproximatelyEqual(Vector3 a, Vector3 b) {
+		return (ApproximatelyEqual(a.x, b.x, 0.01f) && ApproximatelyEqual(a.y, b.y, 0.01f));
+	}
+
+	bool ApproximatelyEqual(float a, float b, float tolerance) {
+		return (Mathf.Abs(a - b) < tolerance);
+	}
+
+	GameObject Furthest() {
+		float dis = -1f;
+		GameObject furthest = bulges[0];
+		for (int i = 0; i < bulges.Length; i++) {
+			float disTemp = (transform.position - bulges[i].transform.position).sqrMagnitude;
+			if (disTemp > dis) {
+				dis = disTemp;
+				furthest = bulges[i];
+			}
+		}
+		return furthest;
+	}
+
 }
---
+++ Assets/Objects/Nozzle.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using System.Collections;
+
+public class Nozzle : MonoBehaviour {
+	float startAngle;
+
+	bool gotWater = true;
+
+	WaterJet waterJet = new WaterJet(8f, 200f, true);
+
+	void Awake() {
+		waterJet.Init();
+		startAngle = transform.eulerAngles.z;
+	}
+
+	void Update() {
+		GameObject ball = GameObject.Find("Ball") as GameObject;
+
+		if (ball != null) {
+			// Rotate towards the ball
+			float angle = Mathf.Atan2(ball.transform.position.y - transform.position.y, ball.transform.position.x - transform.position.x) * Mathf.Rad2Deg;
+			transform.eulerAngles = new Vector3(0f, 0f, angle);
+
+			// Spray the ball
+			if (gotWater) {
+				Vector3 jetOrigin;
+				Vector3 jetVector = waterJet.EulerAngleToNormalizedVector(angle);
+				jetOrigin = transform.position + jetVector * 0.5f;
+				waterJet.Spray(jetOrigin, jetVector);
+			}
+		}
+	}
+}
